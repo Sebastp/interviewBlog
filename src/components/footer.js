9a4398784554a67f7d2637eef6bdbdcb48f53aeb
SEBASTIAN-PC\Sebastian <sebastianpob@gmail.com>
2019-10-07 05:51:15 +0200

    landing grid, footer, col vars, font files

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,13 +4,42 @@ import React from "react"
 const Footer = ({ siteTitle }) => (
   <footer>
     <div className="cont-width_0">
-      <Link to={'/'}>
-        <img src={require('@images/logo.svg')} id="tpb-logoi" alt="logo1"/>
-      </Link>
-      <nav>
-        subreddit
-      </nav>
-      <span>© {new Date().getFullYear()} name</span>
+      <div className="footer-top">
+        <div className="footer-cta">
+          <button className="btn-line__ghost">
+            <img src={require('@images/icons/slack.svg')} className="btn-icon" alt="slack icno"/>
+            Join our Slack
+          </button>
+          <button className="btn-line__ghost">
+            <img src={require('@images/icons/mail.svg')} className="btn-icon" alt="slack icno"/>
+            Contact Us
+          </button>
+        </div>
+
+        <div className="footer-socials">
+          <Link to={'/'} className="button-scm">
+            <img src={require('@images/icons/fb.svg')} alt="social media logo"/>
+          </Link>
+          <Link to={'/'} className="button-scm">
+            <img src={require('@images/icons/twt.svg')} alt="social media logo"/>
+          </Link>
+          <Link to={'/'} className="button-scm">
+            <img src={require('@images/icons/inst.svg')} alt="social media logo"/>
+          </Link>
+        </div>
+      </div>
+
+      <div className="footer-top">
+        <span>© {new Date().getFullYear()}. All rights reserved</span>
+        <div>
+          <Link to={'/'} className="button-text1">
+            Privacy Policy
+          </Link>
+          <Link to={'/'} className="button-text1">
+            Terms of use
+          </Link>
+        </div>
+      </div>
     </div>
   </footer>
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,19 +8,19 @@ import SEO from "@components/seo"
 import ArticleType1 from "@components/posts/articleType1"
 
 const IndexPage = () => (
-  <>
+  <div id="index">
     <SEO title="Home"/>
     <div id="landingTopImage">
       <img src={require('@images/milenialGuy.png')} id="landingTopImage-guy" alt="Milenial Guy"/>
       <img src={require('@images/oninon1.svg')} id="landingTopImage-onion" alt="waves background"/>
     </div>
 
-    <div className="cont-width_0">
+    <div className="cont-width_0" id="landingMainCont">
       <header>
         <Link to={'/'}>
           <img src={require('@images/logo.svg')} id="tpb-logo" alt="logo"/>
         </Link>
-        <div>
+        <div className="landingMainCont-mainHeader">
           <h2>Inspiring Success stories from
             <span>Startup Founders</span>
           </h2>
@@ -31,6 +31,7 @@ const IndexPage = () => (
         <div className="grid-col-left">
 
         </div>
+
         <div className="grid-col-center">
           <ArticleType1/>
         </div>
@@ -38,7 +39,7 @@ const IndexPage = () => (
     </div>
 
     <Footer/>
-  </>
+  </div>
 )
 
 export default IndexPage
